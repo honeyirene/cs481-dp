f057cca5f3eda60449f614f3c96f13f17337b697
daeheon <eogjs99@gmail.com>
2023-04-05 01:17:44 +0900

    타입 다 프린트 해서 적음

--- src/dataStructure/researchData.py
+++ src/dataStructure/researchData.py
@@ -1,14 +1,16 @@
 import numpy
 from typing import Dict, Union, List
 
-Data_X = List[Dict[str, Union[List[int], List[numpy.int64]]]]
-Data_y = numpy.ndarray
-Data_baseline = Dict[str, any]
-Data_pids = numpy.ndarray
-Data_labels = numpy.ndarray
-Data_data_types = numpy.ndarray
+# 사실상 numpy ndarray 인게 많음.
+# ndarray.dtype 이나 ndarray.astype 같은거 활용?
+# 파이썬의 타입은 큰 코드에서 언제나 폭탄이 될 수 있으니 조심.
+
+Data_X = List[Dict[str, Union[List[numpy.float64], List[numpy.int64]]]]
+Data_y = List[List[numpy.int64]]
+Data_baseline = Dict[int, Dict[str, Dict[str,numpy.float64]]]
+Data_pids = List[numpy.int64]
+Data_labels = List[str]
+Data_data_types = List[str]
 
 ResearchDataElement = Union[Data_X,Data_y,Data_baseline,Data_pids,Data_labels,Data_data_types]
 ResearchData = Dict[str, ResearchDataElement]
-
-# keys = 'X', 'y', 'baseline', 'pids', 'labels', 'data_types'
