1236750d8289db8eca00fab115b48f971a31882c
daeheon <eogjs99@gmail.com>
2023-04-04 15:47:43 +0900

    돌아가는 코드 구조

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-import pickleStore
+from store.pickleStore import PickleStore
 
 # 이쪽도 데이터 고치기
-data: any = pickleStore.load()
+def main() -> None:
+    store: PickleStore = PickleStore()
+    data: any = store.load()
+    print(data.keys())
+    return
+
+if __name__ == '__main__':
+    main()
