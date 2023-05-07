d4636bf7d18b2ba1ce16b4368a63248e84c1b06d
daeheon <eogjs99@gmail.com>
2023-05-08 08:18:08 +0900

    대충 손으로 크기 등등 조절

--- src/component/titleComponent.py
+++ src/component/titleComponent.py
@@ -5,7 +5,17 @@ from dash.development.base_component import Component
 # 공용으로 쓸 타이틀 코드. 예쁘게 만드는 건 나중에.
 class TitleComponent:
     def getFC(self, title: str) -> Component:
-        return html.H3(children=title)
+        return html.Span(
+            children=title,
+            style={
+                "font-size": "36px",
+                "font-weight": "bold",
+                "margin-top": "12px",
+                "margin-left": "36px",
+                "margin-right": "36px",
+                "margin-bottom": "12px",
+            },
+        )
 
 
 # 코드 돌아가는지 테스트용
