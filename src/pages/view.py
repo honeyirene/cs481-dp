e0c2e363c7ab2d9ea2651ac23884cb7c6927de47
sy <129829082+sy-opennew@users.noreply.github.com>
2023-05-09 19:50:12 +0900

    Update view.py
    implemented range selector, emotion label, and each value line plots.

--- src/pages/view.py
+++ src/pages/view.py
@@ -6,6 +6,14 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 import plotly.graph_objects as go
 import random
+import plotly
+from dash.dependencies import Input, Output
+#from dash_draggable import Draggable
+
+
+#view = dash.Dash(__name__)
+#def layout():
+#    return html.Div([])
 
 dash.register_page(
     __name__,
@@ -23,60 +31,67 @@ def random_timeseries(initial_value: float, volatility: float, count: int) -> li
         time_series.append(time_series[-1] + initial_value * random.gauss(0, 1)*volatility)
     return time_series
     
-data_x = random_timeseries(0.01, 4, 19200)
-data_y = random_timeseries(0.01, 4, 19200)
-data_z = random_timeseries(0.01, 4, 19200)
-data_bvp = random_timeseries(0.01, 4, 38400)
-data_eda = random_timeseries(0.01, 4, 2400)
+data_x = random_timeseries(0.01, 4, 600) #19200
+data_y = random_timeseries(0.01, 4, 600) #19200
+data_z = random_timeseries(0.01, 4, 600) #19200
+data_bvp = random_timeseries(0.01, 4, 600) #38400
+data_eda = random_timeseries(0.01, 4, 600) #2400
 data_hr_e4 = random_timeseries(0.01, 4, 600)
-data_ibi = random_timeseries(0.01, 4, 38400)
-data_temp = random_timeseries(36.5, 155, 2400)
-data_brainwave_delta = random_timeseries(0.01, 4, 75000)
-data_brainwave_theta = random_timeseries(0.01, 4, 75000)
-data_brainwave_low_alpha = random_timeseries(0.01, 4, 75000)
-data_brainwave_high_alpha = random_timeseries(0.01, 4, 75000)
-data_brainwave_low_beta = random_timeseries(0.01, 4, 75000)
-data_brainwave_high_beta = random_timeseries(0.01, 4, 75000)
-data_brainwave_low_gamma = random_timeseries(0.01, 4, 75000)
-data_brainwave_middle_gamma = random_timeseries(0.01, 4, 75000)
+data_ibi = random_timeseries(0.01, 4, 600) #38400
+data_temp = random_timeseries(36.5, 155, 600) #2400
+data_brainwave_delta = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_theta = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_low_alpha = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_high_alpha = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_low_beta = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_high_beta = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_low_gamma = random_timeseries(0.01, 4, 600) #75000
+data_brainwave_middle_gamma = random_timeseries(0.01, 4, 600) #75000
 data_att = random_timeseries(50, 100, 600)
 data_med = random_timeseries(50, 100, 600)
 data_hr_ecg = random_timeseries(0.01, 4, 600)
 data_audio = random_timeseries(0.01, 4, 600)
 ###########################################################
 ########### create line plot with the data ##################
+#fig = make_subplots(rows=2, cols=1)
 fig_acc = go.Figure()
-fig_acc.add_trace(go.Scatter(y=data_x,
-                        mode='lines',
-                        name='x'))
-fig_acc.add_trace(go.Scatter(y=data_y,
-                         mode='lines',
-                         name='y'))
-fig_acc.add_trace(go.Scatter(y=data_z,
-                         mode='lines',
-                         name='z'))
-fig_acc.update_layout(
-    yaxis_title='g',
-)
+fig_acc.add_trace(go.Scatter(y=data_x, mode='lines', name='x'))
+fig_acc.add_trace(go.Scatter(y=data_y, mode='lines', name='y'))
+fig_acc.add_trace(go.Scatter(y=data_z, mode='lines', name='z'))
+fig_acc.update_layout(yaxis_title='g',
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_bvp = go.Figure()
 fig_bvp.add_trace(go.Scatter(y=data_bvp, mode='lines',name='BVP'))
-fig_acc.update_layout(yaxis_title='PPG')
+fig_bvp.update_layout(yaxis_title='PPG',
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_eda = go.Figure()
 fig_eda.add_trace(go.Scatter(y=data_eda, mode='lines',name='EDA'))
-fig_eda.update_layout(yaxis_title='uS')
+fig_eda.update_layout(yaxis_title='uS',
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_hr = go.Figure()
 fig_hr.add_trace(go.Scatter(y=data_hr_e4, mode='lines',name='E4'))
 fig_hr.add_trace(go.Scatter(y=data_hr_ecg, mode='lines',name='ECG'))
+fig_hr.update_layout(
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_ibi = go.Figure()
 fig_ibi.add_trace(go.Scatter(y=data_ibi, mode='lines',name='IBI'))
+fig_ibi.update_layout(
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_temp = go.Figure()
 fig_temp.add_trace(go.Scatter(y=data_temp, mode='lines',name='Body Temperature'))
-fig_temp.update_layout(yaxis_title='C')
+fig_temp.update_layout(yaxis_title='C',
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_bw = go.Figure()
 fig_bw.add_trace(go.Scatter(y=data_brainwave_delta, mode='lines',name='delta'))
@@ -87,25 +102,137 @@ fig_bw.add_trace(go.Scatter(y=data_brainwave_low_beta, mode='lines',name='low-be
 fig_bw.add_trace(go.Scatter(y=data_brainwave_high_beta, mode='lines',name='high-beta'))
 fig_bw.add_trace(go.Scatter(y=data_brainwave_low_gamma, mode='lines',name='low-gamma'))
 fig_bw.add_trace(go.Scatter(y=data_brainwave_middle_gamma, mode='lines',name='middle-gamma'))
-fig_bw.update_layout(yaxis_title='relative power')
+fig_bw.update_layout(yaxis_title='relative power',
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_att_med = go.Figure()
 fig_att_med.add_trace(go.Scatter(y=data_att, mode='lines',name='attention'))
 fig_att_med.add_trace(go.Scatter(y=data_med, mode='lines',name='meditation'))
+fig_att_med.update_layout(
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 
 fig_audio = go.Figure()
 fig_audio.add_trace(go.Scatter(y=data_audio, mode='lines', name='audio'))
 fig_audio.update_layout(
-    xaxis=
-        dict(
-        rangeslider=
-        dict(
-        visible=True
-        ),
-        )
-)
+    margin=dict(l=1,r=1,t=1,b=1),
+    height=100)
 #######################################################################################
-
+###### emotion label ###################
+emotion_label = [
+        dict(
+            type="rect",
+            xref="x",
+            yref="y",
+            x0="200",
+            y0="-2",
+            x1="300",
+            y1="2",
+            fillcolor="lightslategray",
+            opacity=0.4,
+            line_width=0,
+            layer="below"
+        ),
+        dict(
+            type="rect",
+            xref="x",
+            yref="y",
+            x0="500",
+            y0="-2",
+            x1="600",
+            y1="2",
+            fillcolor="gray",
+            opacity=0.4,
+            line_width=0,
+            layer="below"
+        ),
+        dict(
+            type="rect",
+            xref="x",
+            yref="y",
+            x0="0",
+            y0="-2",
+            x1="100",
+            y1="2",
+            fillcolor="black",
+            opacity=0.4,
+            line_width=0,
+            layer="below"
+        ),
+        dict(
+            type="rect",
+            xref="x",
+            yref="y",
+            x0="100",
+            y0="-2",
+            x1="200",
+            y1="2",
+            fillcolor="darkslategray",
+            opacity=0.4,
+            line_width=0,
+            layer="below"
+        ),
+        dict(
+            type="rect",
+            xref="x",
+            yref="y",
+            x0="400",
+            y0="-2",
+            x1="500",
+            y1="2",
+            fillcolor="lightslategray",
+            opacity=0.4,
+            line_width=0,
+            layer="below"
+        ),
+    ]
+fig_acc.update_layout(shapes=emotion_label)
+fig_acc.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_bvp.update_layout(shapes=emotion_label)
+fig_bvp.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_eda.update_layout(shapes=emotion_label)
+fig_eda.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_hr.update_layout(shapes=emotion_label)
+fig_hr.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_ibi.update_layout(shapes=emotion_label)
+fig_ibi.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_temp.update_layout(shapes=emotion_label)
+fig_temp.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_att_med.update_layout(shapes=emotion_label)
+fig_att_med.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_bw.update_layout(shapes=emotion_label)
+fig_bw.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+fig_audio.update_layout(shapes=emotion_label)
+fig_audio.update_layout({
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'paper_bgcolor': 'rgba(0,0,0,0)'
+})
+################################################
 ################ style #########################
 colors = {
     'deep color': '#1091DB',
@@ -136,7 +263,7 @@ layout = html.Div(
                         dbc.Col(children = [
                             html.Img(src="https://i.ibb.co/ctFP3Sg/1.png", style={'height': 150, 'width': 240}), html.Br(),
                             #dcc.Graph(figure=fig_audio.rangeslider),
-                            html.Div(dcc.RangeSlider(0, 600, marks=None, value=[0, 600],
+                            html.Div(dcc.RangeSlider(id='time_slider', min=0, max=600, marks=None, value=[0, 600],
                                                          tooltip={"placement":"bottom", "always_visible":True}))])
                         ]
                 )
@@ -144,53 +271,74 @@ layout = html.Div(
         html.Div(html.Div(children=html.B("Data View"), style={'color': colors['deep color']})),
         html.Div(html.Br()),
         html.Div(
+            #[
+                #Draggable(
             dbc.Accordion(
                 [
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_acc),
-                        title="3-axis Acceleration",
+                        dcc.Graph(figure=fig_acc, id='acc'),
+                        title="3-axis Acceleration"
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_bvp),
+                        dcc.Graph(figure=fig_bvp, id='bvp'),
                         title="BVP",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_eda),
+                        dcc.Graph(figure=fig_eda, id='eda'),
                         title="EDA",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_hr),
+                        dcc.Graph(figure=fig_hr, id='hr'),
                         title="Heart Rates",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_ibi),
+                        dcc.Graph(figure=fig_ibi, id='ibi'),
                         title="IBI",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_temp),
+                        dcc.Graph(figure=fig_temp, id='temp'),
                         title="Body Temperature",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_att_med),
+                        dcc.Graph(figure=fig_att_med, id='a&m'),
                         title="Attention and Meditation",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_bw),
+                        dcc.Graph(figure=fig_bw, id='bw'),
                         title="BrainWave",
                     ),
                     dbc.AccordionItem(
-                        dcc.Graph(figure=fig_audio),
+                        dcc.Graph(figure=fig_audio, id='audio'),
                         title="Audio",
                     ),
                 ],
                 always_open=True,
             )
         )
+            ])
 
-    ]
-)
+    #]
+#)
+"""
+@view.callback(
+    [dash.dependencies.Output('acc', 'figure'),
+     dash.dependencies.Output('bvp', 'figure'),
+    # dash.dependencies.Output('eda', 'figure'),
+    # dash.dependencies.Output('hr', 'figure'),
+    # dash.dependencies.Output('ibi', 'figure'),
+    # dash.dependencies.Output('temp', 'figure'),
+    # dash.dependencies.Output('a&m', 'figure'),
+    # dash.dependencies.Output('bw', 'figure'),
+    # dash.dependencies.Output('audio', 'figure')],
+    ],[dash.dependencies.Input('time_slider', 'value')]
+)"""
+def update_plots(active_range):
+    for fig in [fig_acc, fig_bvp]:
+        fig.update_layout(xaxis=dict(range=active_range))
+    return fig_acc, fig_bvp
 
 
 # 코드 돌아가는지 테스트용
 if __name__ == "__main__":
+    #view.run_server(debug=True)
     print("skip")
