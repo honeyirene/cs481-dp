468b5e1a0bb18452699bf5c498ff8dd68053690d
daeheon.han <eogjs99@gmail.com>
2023-05-21 05:39:43 +0900

    navbar 지우고 전체 구조 고치기 (#18)

--- src/component/titleComponent.py
+++ src/component/titleComponent.py
@@ -5,15 +5,16 @@ from dash.development.base_component import Component
 # 공용으로 쓸 타이틀 코드. 예쁘게 만드는 건 나중에.
 class TitleComponent:
     def getFC(self, title: str) -> Component:
-        return html.Span(
+        return html.Div(
             children=title,
             style={
+                "height": "3%",
                 "font-size": "36px",
                 "font-weight": "bold",
-                "margin-top": "12px",
+                # "margin-top": "12px",
                 "margin-left": "36px",
                 "margin-right": "36px",
-                "margin-bottom": "12px",
+                # "margin-bottom": "12px",
             },
         )
 
