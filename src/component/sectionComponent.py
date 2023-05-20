f775c027b828cbcadf4d4d5a2ad3df92199458ea
daeheon <eogjs99@gmail.com>
2023-05-21 01:26:27 +0900

    코드 구조 정리 + 버튼 위치 옮김

--- src/component/sectionComponent.py
+++ src/component/sectionComponent.py
@@ -5,26 +5,28 @@ from dash import dcc, html, callback, Input, Output
 
 
 class SectionComponent:
-    isVideoZoom = True
-    changePortionButton = html.Button("change", id="input", n_clicks=0)
+    def __upperBar(self) -> Component:
+        upperLeft = html.Div(id="upper-left-empty", style={"width": "95%"})
+        changePortionButton = html.Div(html.Button("change", id="input", n_clicks=0))
+        return html.Div([upperLeft, changePortionButton], style={"display": "flex"})
 
-    def getFC(
+    def __mainContainer(
         self,
         leftUpperChildren: Component,
         leftLowerChildren: Component,
         rightChildren: Component,
     ) -> Component:
         leftUpper = html.Div(id="left-upper-panel", children=leftUpperChildren)
+        leftMiddle = html.Div(id="left-middle-gap", style={"height": "15px"})
         leftLower = html.Div(id="left-lower-panel", children=leftLowerChildren)
         left = html.Div(
             id="left-panel",
             children=[
                 leftUpper,
-                self.changePortionButton,
+                leftMiddle,
                 leftLower,
             ],
         )
-
         middle = html.Div(id="middle-gap", style={"width": "1%"})
         right = html.Div(id="right-panel", children=rightChildren)
 
@@ -37,8 +39,27 @@ class SectionComponent:
             style={"display": "flex"},
         )
 
+    def getFC(
+        self,
+        leftUpperChildren: Component,
+        leftLowerChildren: Component,
+        rightChildren: Component,
+    ) -> Component:
+        return html.Div(
+            children=[
+                self.__upperBar(),
+                self.__mainContainer(
+                    leftUpperChildren,
+                    leftLowerChildren,
+                    rightChildren,
+                ),
+            ]
+        )
+
     @callback(
         Output("left-panel", "style"),
+        Output("left-upper-panel", "style"),
+        Output("left-lower-panel", "style"),
         Output("right-panel", "style"),
         Input("input", "n_clicks"),
     )
@@ -47,20 +68,32 @@ class SectionComponent:
         if (n_clicks) % 2 == 0:
             leftStyle = {
                 "width": "35%",
-                "border": "1px solid cyan",
             }
             rightStyle = {
-                "width": " 64%",
+                "width": "64%",
+                "border": "1px solid cyan",
+            }
+            leftUpperStyle = {
+                "height": "300px",
+                "border": "1px solid cyan",
+            }
+            leftLowerStyle = {
                 "border": "1px solid cyan",
             }
         else:
             leftStyle = {
                 "width": "49.5%",
-                "border": "1px solid cyan",
             }
             rightStyle = {
                 "width": "49.5%",
                 "border": "1px solid cyan",
             }
+            leftUpperStyle = {
+                "height": "500px",
+                "border": "1px solid cyan",
+            }
+            leftLowerStyle = {
+                "border": "1px solid cyan",
+            }
 
-        return leftStyle, rightStyle
+        return leftStyle, leftUpperStyle, leftLowerStyle, rightStyle
