9154516d9329af50d1d91e494c13c670ee663052
daeheon <eogjs99@gmail.com>
2023-05-22 03:55:14 +0900

    배포시도

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 import dash
 import dash_bootstrap_components as dbc
-from component.navbarComponent import NavbarComponent
+
 
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
 app.layout = dbc.Container(dash.page_container, fluid=True, style={"height": "100vh"})
+server = app.server
 
 if __name__ == "__main__":
     app.run_server(debug=True)
