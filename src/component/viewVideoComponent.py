c25576fe8d3afc9e0fd12df09e4e92e749efaf76
daeheon <eogjs99@gmail.com>
2023-06-05 00:23:27 +0900

    코드정리

--- src/component/viewVideoComponent.py
+++ src/component/viewVideoComponent.py
@@ -1,7 +1,5 @@
-import dash
 import dash_player as dp
 from dash import html
-from dash.dependencies import Input, Output
 from dash.development.base_component import Component
 
 
@@ -14,22 +12,6 @@ class ViewVideoComponent:
             width="100%",
             height="100%",
         )
-
-        app = dash.get_app()
-
-        @app.callback(
-            [
-                # 여기에 다른 파일에 있는 element의 id를 그대로 써서 연결.
-                Output("player", "currentTime"),
-            ],
-            [
-                Input("player", "currentTime"),
-            ],
-        )
-        def update(currentTime):
-            # print(type(currentTime))
-            # print(currentTime)
-            return [currentTime]
 
         return html.Div(
             children=[video],
